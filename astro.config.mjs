ab13e72616f844b274e2be6e95206f2ba7d432f1
maaualejandrx <alexsantiflores03@gmail.com>
2026-05-30 19:39:48 -0600

    config yml

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,3 +1,6 @@
 import { defineConfig } from 'astro/config';
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+     site: 'https://github.io',
+  base: '/LuzDental', 
+});
